4b026c5bbeb420fe720fc88270b49f2810ca2a77
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-10-08 16:35:10 +0400

    Add upcoming events report to ReportsController
    - Introduced new using directives for DTOs, services, and Swagger.
    - Marked ReportsController with [AllowAnonymous] for public access.
    - Added Upcoming method to retrieve upcoming events with Swagger documentation.

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -1,13 +1,16 @@
-﻿using EventManagementAPI.Repo;
+﻿using EventManagementAPI.DTOs;
+using EventManagementAPI.Repo;
 using EventManagementAPI.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace EventManagementAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     [AllowAnonymous]
+   
     public class ReportsController : ControllerBase
     {
         private readonly IEventReportService _reportService;
@@ -18,6 +21,10 @@ namespace EventManagementAPI.Controllers
         }
 
         [HttpGet("upcoming-events")]
+        [AllowAnonymous]
+        //Swagger documentation
+        [SwaggerOperation(Summary = "Upcoming events report")]
+        [ProducesResponseType(typeof(IEnumerable<UpcomingEventReportDto>), 200)]
         public async Task<IActionResult> Upcoming([FromQuery] int days = 30)
         {
             var data = await _reportService.GetUpcomingWithWeatherAsync(days);
